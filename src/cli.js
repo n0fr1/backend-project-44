f81df55237e9b32c28ce1cff74fb8c6814c56808
Evgeniy <n0fr1@mail.ru>
2023-09-25 22:25:10 +0300

    add linter and prettier

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,9 @@
-import readlinesync from 'readline-sync'
+import readlinesync from 'readline-sync';
 
 const greetings = () => {
-    console.log(`Welcome to the Brain games!`);
-    const name = readlinesync.question('May I have your name?');
-    console.log(`Hello, ${name}!`);    
+  console.log('Welcome to the Brain games!');
+  const name = readlinesync.question('May I have your name?');
+  console.log(`Hello, ${name}!`);
 };
 
 export default greetings;
