51e3b4ff2f9f8bf42556f750650ffdb9e0a84385
Evgeniy <n0fr1@mail.ru>
2023-09-27 17:11:42 +0300

    changed brain-even as executable

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,38 +1,4 @@
-import readlinesync from 'readline-sync';
+#!/usr/bin/env node
+import evenGame from '../src/games/even.js';
 
-const getRandomInt = (maxInt) => Math.floor(Math.random() * maxInt);
-
-const testEven = (randItem) => (randItem % 2 === 0 ? 'yes' : 'no');
-
-const wrongAnswerText = (answerUser, rightAnswer, userName) => {
-  console.log(`'${answerUser}' is wrong answer ;(. Correct answer was '${rightAnswer}'.`);
-  console.log(`Let's try again, '${userName}'!`);
-};
-
-const evenGame = (userName) => {
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  let countRound = 0;
-  let countRightAnswers = 0;
-  const roundsGame = 3;
-  const maxInt = 100;
-  let answerUser;
-  let rightAnswer;
-  do {
-    countRound += 1;
-    const randItem = getRandomInt(maxInt);
-    console.log(`Question: ${randItem}`);
-    rightAnswer = testEven(randItem);
-    answerUser = readlinesync.question('Your answer:');
-    if (answerUser === rightAnswer) {
-      console.log('Correct!');
-      countRightAnswers += 1;
-    } else {
-      wrongAnswerText(answerUser, rightAnswer, userName);
-    }
-  } while ((countRound < roundsGame) && (answerUser === rightAnswer));
-  if (countRightAnswers === roundsGame) {
-    console.log(`Congratulations, ${userName}!`);
-  }
-};
-
-export default evenGame;
+evenGame();
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,4 @@
 #!/usr/bin/env node
 import greetings from '../src/cli.js';
-import evenGame from './brain-even.js';
 
-const userName = greetings();
-evenGame(userName);
+greetings();
