5b0a24cfb84555ba42aa05b42d9b7309027cdaa6
Evgeniy <n0fr1@mail.ru>
2023-10-12 22:32:39 +0300

    refactored 3

--- src/games/even.js
+++ src/games/even.js
@@ -1,5 +1,5 @@
 import startGame from '../index.js';
-import { getRandomInt, getDivider } from '../utils.js';
+import { getRandomInt, isDivisible } from '../utils.js';
 
 const maxIntEven = 100;
 const minIntEven = 1;
@@ -7,7 +7,7 @@ const minIntEven = 1;
 const getRoundEven = () => {
   const randNum = getRandomInt(maxIntEven, minIntEven);
   const curQuestion = `Question: ${randNum}`;
-  const curAnswer = (getDivider(randNum, 2)) ? 'yes' : 'no';
+  const curAnswer = (isDivisible(randNum, 2)) ? 'yes' : 'no';
   return [curQuestion, curAnswer];
 };
 
--- src/utils.js
+++ src/utils.js
@@ -2,12 +2,8 @@ import readlinesync from 'readline-sync';
 
 const getRandomInt = (limitRandom, minNum) => (Math.floor(Math.random() * limitRandom) + minNum);
 
-// Есть негласное правило, что переменные и функции, которые содержат/возвращают булевы значения, лучше называть начиная с 'is/has'.
-// Это улучшает читаемость кода, потому что можно с одного взгляда определить, что здесь идет работа с булевыми значениями.
-// Например, здесь у тебя функция отвечает на вопрос "Делится ли одно число на другое без остатка?" -- "isDivisible"
-const getDivider = (num, divider) => (num % divider === 0);
+const isDivisible = (num, divider) => (num % divider === 0);
 
-// Эта функция используется для получения и ответа, и имени юзера. Можно ее назвать getUserInput, а аргумент -- prompt.
-const getUserAnswer = (userQuestion) => readlinesync.question(userQuestion);
+const getUserInput = (prompt) => readlinesync.question(prompt);
 
-export { getRandomInt, getDivider, getUserAnswer };
+export { getRandomInt, isDivisible, getUserInput };
