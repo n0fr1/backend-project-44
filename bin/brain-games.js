01b0145166c9598f3d746a9fcbe12e8706faef4d
Evgeniy <n0fr1@mail.ru>
2023-10-12 23:04:51 +0300

    corrected brain-games

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-import { getUserAnswer } from '../src/utils.js';
+import { getUserInput } from '../src/utils.js';
 
 console.log('Welcome to the Brain Games!');
-const userName = getUserAnswer('May I have your name?');
+const userName = getUserInput('May I have your name?');
 console.log(`Hello, ${userName}!`);
